5d3493e75f39fcecf454d35abdb45925d56ede30
gabriel <165610905+Gabriel-Ax@users.noreply.github.com>
2025-10-14 20:23:13 -0300

    feat: add new operations & function to run the program

--- app/src/main/java/org/example/Calculator.java
+++ app/src/main/java/org/example/Calculator.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Calculator {
 
  private int leftOperand; // NOTE: aka, {2} * 3
@@ -9,24 +11,41 @@ public class Calculator {
 
  private int currentValue;
 
+ public Calculator() {
+  this.currentValue = 0;
+ }
+
  // NOTE: it would be interesting to use enum functions to
  // "apply" operations
  public enum Operation {
-  SUM("+", 1) {
+  SUM("+", 1) { // WARN: It was really tiresome to modify id
+                // by id at the addition of a new operation
    @Override
-   public int Apply(int a, int b) {
+   public int apply(int a, int b) {
     return a + b;
    }
   },
-  MULTIPLY("*", 2) {
+  SUB("-", 2) {
    @Override
-   public int Apply(int a, int b) {
+   public int apply(int a, int b) {
+    return a - b;
+   }
+  },
+  MULTIPLY("*", 3) {
+   @Override
+   public int apply(int a, int b) {
     return a * b;
    }
   },
-  POWER("^", 3) {
+  DIVIDE("/", 4) {
    @Override
-   public int Apply(int a, int b) {
+   public int apply(int a, int b) {
+    return a / b;
+   }
+  },
+  POWER("^", 5) {
+   @Override
+   public int apply(int a, int b) {
 
     int value = a;
     for (int i = 1; i < b; i++) { // NOTE: Power starts at 1 and not 0
@@ -34,6 +53,52 @@ public class Calculator {
     }
 
     return value;
+   }
+  },
+  ROOT("√", 6) {
+   @Override
+   public int apply(int a, int b) {
+    // NOTE: I dont understand exactly how this works, but
+    // i have seen it from more them 1 source
+
+    if (a == 0)
+     return 0;
+    if (a == 1)
+     return 1;
+    if (b == 0)
+     return 1;
+
+    int low = 1;
+    int high = a;
+    int result = 1;
+
+    while (low <= high) {
+     int mid = low + (high - low) / 2;
+
+     int power = 1;
+     boolean overflow = false;
+     for (int i = 0; i < b; i++) {
+      if (power > a / mid) {
+       overflow = true;
+       break;
+      }
+      power = power * mid;
+     }
+     if (overflow || power > a) {
+      high = mid - 1;
+     } else if (power < a) {
+      result = mid;
+      low = mid + 1;
+     } else {
+      return mid;
+     }
+    }
+
+    return result;
+   }
+
+   public int apply(int a) {
+    return this.apply(a, 2);
    }
   };
 
@@ -45,7 +110,7 @@ public class Calculator {
    this.id = id;
   }
 
-  public abstract int Apply(int a, int b);
+  public abstract int apply(int a, int b);
 
   // WARN: Beware the pratice of adding stuff freely without
   // testing
@@ -66,8 +131,44 @@ public class Calculator {
   }
  }
 
- public Calculator() {
-  this.currentValue = 0;
+ public int enterNumber(Scanner scanner) {
+  String s = scanner.next(); // WARN: The user could input a letter, symbol or more them 1 number
+  int i = Integer.parseInt(s);
+  return i;
+ }
+
+ public void run() {
+  // WARN: there is a tinisy possibility that im missing and this
+  // loop is capable of running forever
+  boolean loop = true;
+  while (true) {
+
+   int i = 0;
+
+   // WARN: This wont fit a test
+   Scanner scanner = new Scanner(System.in); // PERF: We could inject this scanner
+
+   System.out.println("Please select a operator:");
+   for (Operation op : Operation.values()) {
+    System.out.println(op.id + ": " + op.symbol);
+   }
+
+   i = enterNumber(scanner);
+   this.selectOperator(i);
+
+   System.out.print("The left operand: ");
+   i = this.enterNumber(scanner);
+   this.setLeftOperand(i);
+
+   System.out.print("\nThe right operand: ");
+   i = this.enterNumber(scanner);
+   this.setRightOperand(i);
+
+   this.applyOpperation();
+   System.out.println("Final result: " + this.Result());
+
+   continue;
+  }
  }
 
  public int getLeftOperand() {
@@ -91,54 +192,29 @@ public class Calculator {
   System.out.println("1: + (sum)");
   System.out.println("2: * (multiply)");
   System.out.println("3: ^ (power)");
+ }
 
-  int choice = 0;
-  Operation operation = Operation.fromId(choice);
-  switch (operation) {
-   case SUM:
-    this.Multiplicate(a, b);
-    break;
-
-   case MULTIPLY:
-    this.Multiplicate(a, b);
-    break;
-
-   case POWER:
-    this.Power(a, b);
-    break;
-
-   default:
-    throw new IllegalArgumentException("Invalid operation ID: " + choice);
+ public void selectOperator(int id) {
+  for (Operation op : Operation.values()) {
+   if (op.id == id) {
+    this.setOperator(op);
+    return;
+   }
   }
+  // WARN: Maybe its a bit overkill to just throw an exception...
+  throw new IllegalArgumentException("A operation with the given ID " + id + " doesnt exist");
+ }
+
+ public Operation getOperator() {
+  return this.operator;
  }
 
  public void setOperator(Operation operator) {
   this.operator = operator;
  }
 
- public void ApplyOpperation() {
-  this.currentValue = this.operator.Apply(this.leftOperand, this.rightOperand);
- }
-
- public void Sum(int a, int b) {
-  int value = a + b;
-  this.currentValue = this.currentValue + value;
- }
-
- public void Multiplicate(int a, int b) {
-  int value = a * b;
-  this.currentValue = value;
- }
-
- public void Power(int a, int b) {
-
-  int value = a;
-
-  // for (int i = 0; i < b; i++) {
-  for (int i = 1; i < b; i++) { // NOTE: Power starts at 1 and not 0
-   value = value * a;
-  }
-  this.currentValue = value;
+ public void applyOpperation() {
+  this.currentValue = this.operator.apply(this.leftOperand, this.rightOperand);
  }
 
  public int Result() {
